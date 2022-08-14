672dd3c447ac9e4e2f9e1ee5de0af1788e66d311
djorges <jorgesdiego@hotmail.com>
2022-08-14 20:01:35 -0300

    Empty Project Final

--- app/src/main/java/com/example/uicompose/domain/model/Task.kt
+++
@@ -1,7 +0,0 @@
-package com.example.uicompose.domain.model
-
-data class Task(
-    val id:Int,
-    val message:String,
-    var checked: Boolean = false
-)
--- app/src/main/java/com/example/uicompose/presentation/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/uicompose/presentation/ui/screen/HomeScreen.kt
@@ -1,6 +1,13 @@
 package com.example.uicompose.presentation.ui.screen
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.uicompose.presentation.viewmodel.MainViewModel
 
@@ -9,4 +16,11 @@ fun HomeScreen(
     navController: NavHostController,
     viewModel: MainViewModel
 ) {
+    Text(
+        text = "this is a text",
+        modifier = Modifier
+            .fillMaxWidth(),
+        fontSize = 22.sp,
+        textAlign = TextAlign.Center
+    )
 }
